7ae75f5e999fb732f2d07cf2ff877942d6a36ffd
Wiljan Vinke <wiljanvinke@gmail.com>
2020-01-09 14:28:13 +0100

    Updated todo-reminder to include concealment

--- src/game/Stratego.java
+++ src/game/Stratego.java
@@ -4,4 +4,7 @@ public class Stratego {
     public static void main(String[] args){
         Game game = new Game();
     }
+    //TODO wincondition
+    //TODO unique piece behaviour
+    //TODO concealment
 }
