d2e29b0f90d9dc6a40420edddd8520d334033b2f
Wiljan Vinke <wiljanvinke@gmail.com>
2020-01-14 14:34:45 +0100

    Fixed NullPointer when moving up, right or left on or over empty Fields

--- src/game/pieces/Bomb.java
+++ src/game/pieces/Bomb.java
@@ -3,7 +3,10 @@ package game.pieces;
 import game.Player;
 
 public class Bomb extends Piece {
+    private int range = 0;
+
     public Bomb(Player player){
         super(player, 11);
+        setRange(range);
     }
 }
--- src/game/pieces/Flag.java
+++ src/game/pieces/Flag.java
@@ -3,7 +3,10 @@ package game.pieces;
 import game.Player;
 
 public class Flag extends Piece {
+    private int range = 0;
+
     public Flag(Player player){
         super(player, 0);
+        setRange(range);
     }
 }
