3df5d4368dd8f2aac35b48fd3cbc39d545b7e7ac
Wiljan Vinke <wiljanvinke@gmail.com>
2020-01-14 14:33:54 +0100

    Fixed Range of Scout

--- src/game/pieces/Scout.java
+++ src/game/pieces/Scout.java
@@ -3,14 +3,10 @@ package game.pieces;
 import game.Player;
 
 public class Scout extends Piece {
-    private int range = 10;
+    private int range = 9;
 
     public Scout(Player player){
         super(player, 2);
+        setRange(range);
     }
-
-/*    @Override
-    public int getRange() {
-        return range;
-    }*/
 }
