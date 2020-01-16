94acc6fbf2313ae4ce7793eb987dc0ff532501f8
Wiljan Vinke <wiljanvinke@gmail.com>
2020-01-16 10:58:48 +0100

    Changed Rank to protected

--- src/game/pieces/Spy.java
+++ src/game/pieces/Spy.java
@@ -1,9 +1,19 @@
 package game.pieces;
 
 import game.Player;
+import game.extra.Rank;
 
 public class Spy extends Piece {
     public Spy(Player player){
         super(player, 1);
     }
+
+    @Override
+    public int compare(Piece piece){
+        if (piece.getRank() == Rank.MARSHALL){
+            return 10;
+        }
+        return rank.toInt() - piece.getRank().toInt();
+    }
+
 }
