fbe0c94cf0296622736c21a023cd93693a953ec8
Wiljan Vinke <wiljanvinke@gmail.com>
2020-02-06 11:59:22 +0100

    Moved gamelogic from constructor to start()

--- src/game/Stratego.java
+++ src/game/Stratego.java
@@ -3,6 +3,7 @@ package game;
 public class Stratego {
     public static void main(String[] args){
         Game game = new Game();
+        game.start();
     }
     //TODO concealment
     //TODO printmove()
